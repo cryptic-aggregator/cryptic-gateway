d83bfdca29c4990cedc1aa24f49a500693c34fc2
Stanislav Molchan <stasek.molchan@gmail.com>
2025-02-24 22:05:38 +0200

    add cors

--- GatewayService/GatewayService/Program.cs
+++ GatewayService/GatewayService/Program.cs
@@ -23,6 +23,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,6 +32,12 @@ app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
 });
+
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials()
+    .SetIsOriginAllowed(origin => true));
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
