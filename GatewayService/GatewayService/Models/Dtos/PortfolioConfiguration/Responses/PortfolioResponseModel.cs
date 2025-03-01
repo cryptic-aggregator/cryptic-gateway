de2944d0058170febdfc57f5b931e52a6d2c63d0
Stanislav Molchan <stasek.molchan@gmail.com>
2025-03-01 17:28:17 +0200

    fix

---
+++ GatewayService/GatewayService/Models/Dtos/PortfolioConfiguration/Responses/PortfolioResponseModel.cs
@@ -0,0 +1,19 @@
+using Newtonsoft.Json;
+
+namespace GatewayService.Models.Dtos.PortfolioConfiguration.Responses;
+
+public class PortfolioResponseModel
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+        
+    [JsonProperty("name")]
+    public string Name { get; set; }
+        
+    [JsonProperty("owner_id")]
+    public int OwnerId { get; set; }
+        
+    // Unix timestamp у секундах
+    [JsonProperty("created_at")]
+    public long CreatedAt { get; set; }
+}
