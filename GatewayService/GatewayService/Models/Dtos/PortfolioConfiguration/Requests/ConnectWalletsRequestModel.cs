b8c113dc9ebac7a6eedc497222b4b1e932cfa577
Stanislav Molchan <stasek.molchan@gmail.com>
2025-05-26 12:10:13 +0300

    Update JSON serialization attributes in wallet models
    Replaced `[JsonPropertyName]` with `[JsonProperty]` to ensure compatibility with the JSON serialization library being used. This change enhances alignment with expected serialization behaviors and improves overall data handling.

--- GatewayService/GatewayService/Models/Dtos/PortfolioConfiguration/Requests/ConnectWalletsRequestModel.cs
+++ GatewayService/GatewayService/Models/Dtos/PortfolioConfiguration/Requests/ConnectWalletsRequestModel.cs
@@ -7,23 +7,29 @@ namespace GatewayService.Models.Dtos.PortfolioConfiguration.Requests;
 public class WalletConnectEntityModel
 {
     [JsonPropertyName("name")]
+    [JsonProperty("name")]
     public string Name { get; set; }
         
     [JsonPropertyName("caip_address")]
+    [JsonProperty("caip_address")]
     public string CaipAddress { get; set; }
         
     [JsonPropertyName("connector")]
+    [JsonProperty("connector")]
     public string Connector { get; set; }
         
     [JsonPropertyName("connection_type")]
+    [JsonProperty("connection_type")]
     public WalletConnectionType ConnectionType { get; set; }
         
     [JsonPropertyName("wallet_address")]
+    [JsonProperty("wallet_address")]
     public string WalletAddress { get; set; }
 }
 
 public class ConnectWalletsRequestModel
 {
     [JsonPropertyName("wallets")]
+    [JsonProperty("wallets")]
     public List<WalletConnectEntityModel> Wallets { get; set; }
 }
